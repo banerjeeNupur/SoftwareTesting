e7e0d4b5948c8edfff69b7a493e6dbccb97b3e46
Meghna Dubey <meghnadubey300@gmail.com>
2021-11-25 22:36:35 +0530

    Magic Square tests added

---
+++ src/test/java/com/stp/Maths/MagicSquareTest.java
@@ -0,0 +1,30 @@
+package com.stp.Maths;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MagicSquareTest {
+//276 951 438
+    MagicSquare m = new MagicSquare();
+    @BeforeEach
+    void setUp(){
+    }
+
+    @AfterEach
+    void tearDown(){
+    }
+
+    @Test
+    void magicSquareTest(){
+        int [][] arr1 = {{2,7,6},{9,5,1},{4,3,8}};
+        assertTrue(Arrays.deepEquals(arr1, m.magicsquare(3)));
+        assertFalse(Arrays.deepEquals(arr1,m.magicsquare(2)));
+        assertNull(m.magicsquare(0));
+        assertNotNull(m.magicsquare(5));
+    }
+}
